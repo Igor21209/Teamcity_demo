990e8d100e0eee77eaa67fd7a3aa4aaa1916c6e0
root <root@NB1936.inside.glowbyte.com>
2022-12-05 17:48:39 +0300

    main

---
+++ TeamCity_demo/ALL/DDL/customer.sql
@@ -0,0 +1,10 @@
+whenever sqlerror exit sql.sqlcode
+declare
+   c int;
+begin
+   select count(*) into c from user_tables where table_name = upper('temporar_table');
+   if c = 0 then
+        execute immediate 'CREATE TABLE customer (id INT,  last_name varchar(64),   first_name varchar(64),   middle_name varchar(64),   birth_date DATE)';
+   end if;
+end;
+exit;
---
+++ TeamCity_demo/ALL/SAS/macros_2.sas
@@ -0,0 +1,1 @@
+%put macros_2;
