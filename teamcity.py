99eb197f58b6f4da1c0d0e73afc82d4b5188a49b
Бородин Игорь <igor.borodin@inside.glowbyte.com>
2022-12-12 17:40:13 +0300

    release_1

--- teamcity.py
+++ teamcity.py
@@ -115,7 +115,7 @@ END My_Types;
         data = self.yaml_parser(self.path_to_yaml)
         self.execute_files(data)
         test = self.yaml_parser(self.path_to_yaml).get('patch')
-        get_pathes_for_insall(test)
+        self.get_pathes_for_insall(test)
 
 
 
