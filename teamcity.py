abdb650112bd6a6bc2bd72e00452c9e41d76a963
Бородин Игорь <igor.borodin@inside.glowbyte.com>
2022-12-14 18:35:19 +0300

    release_1

--- teamcity.py
+++ teamcity.py
@@ -123,15 +123,15 @@ class Teamcity:
             branch = re.search('Merge: .+ (.+)', get_branch).group(1)
             #print(branch)
             get_branch = self.run_shell_command(f'git show {branch}')
-            print(get_branch)
-            '''
-            branch = re.search('\((.+)\)', get_branch).group(1)
-            print(branch, 'HERE I AM!')
+            #print(get_branch)
+
+            #branch = re.search('\((.+)\)', get_branch).group(1)
+            #print(branch, 'HERE I AM!')
             commit_version = re.search('commit (........................................) ', get_branch).group(1)
             print(commit_version)
-            date = re.search('Date: (.+)', get_branch).group(1).strip()
-            print(date)
-            '''
+            #date = re.search('Date: (.+)', get_branch).group(1).strip()
+            #print(date)
+
 
 
 
