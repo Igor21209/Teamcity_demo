5668a0953a234e6a0c78a39ced7eccfe6d8c9b1e
Бородин Игорь <igor.borodin@inside.glowbyte.com>
2022-12-14 18:37:01 +0300

    release_1

--- teamcity.py
+++ teamcity.py
@@ -127,7 +127,7 @@ class Teamcity:
 
             #branch = re.search('\((.+)\)', get_branch).group(1)
             #print(branch, 'HERE I AM!')
-            commit_version = re.search('commit (........................................) ', get_branch).group(1)
+            commit_version = re.search('commit', get_branch).group(1)
             print(commit_version)
             #date = re.search('Date: (.+)', get_branch).group(1).strip()
             #print(date)
