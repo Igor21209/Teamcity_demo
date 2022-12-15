be6fbf390b2ffa06bee4ab00188bdce0956dc04b
Бородин Игорь <igor.borodin@inside.glowbyte.com>
2022-12-15 23:11:45 +0300

    release_5 fix

--- teamcity.py
+++ teamcity.py
@@ -158,10 +158,14 @@ class Teamcity:
             for commit in list_of_commits:
                 branch = f'git show {commit}'
                 get_branch = self.run_shell_command(branch)
+                print(get_branch)
                 branch_1 = re.search('Merge: .+ (.+)', get_branch).group(1)
+                print(branch_1)
                 date = re.search('Date: (.+)', get_branch).group(1).strip()
                 name_of_branch = self.run_shell_command(f'git name-rev {branch_1}')
                 branch_name = re.search('.+ (.+)', name_of_branch).group(1)
+                print(branch_name)
+                print(name_of_branch)
                 if branch_name == patch_name:
                     commit_list.append(Commit(commit, date, branch_name))
         commit_list.sort(reverse=False, key=self.sort)
