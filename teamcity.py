caa2a923720ef30ab1654796ac95482f708a10f1
Бородин Игорь <igor.borodin@inside.glowbyte.com>
2022-12-15 10:29:15 +0300

    release_1

--- teamcity.py
+++ teamcity.py
@@ -97,7 +97,6 @@ class Teamcity:
     def run_shell_command(self, command):
         process = Popen(args=command, stdout=PIPE, shell=True)
         if process.communicate():
-            print(process.communicate()[0])
             return process.communicate()[0].decode('UTF-8')
 
     def get_commit_version(self, sql_path):
