654fa001fb0885a1e4c0dcb4a527eb1f165228ea
Бородин Игорь <igor.borodin@inside.glowbyte.com>
2022-12-16 12:20:39 +0300

    release_5 fix

--- teamcity.py
+++ teamcity.py
@@ -99,9 +99,9 @@ class Teamcity:
                 if sas:
                     for s in sas:
                         self.ssh_copy(s, self.target_dir)
-                add_to_install_patches = f"MERGE INTO PATCH_STATUS USING DUAL ON (PATCH_NAME = {patch.branch})\
+                add_to_install_patches = f"MERGE INTO PATCH_STATUS USING DUAL ON (PATCH_NAME = '{patch.branch}')\
                 \nWHEN NOT MATCHED THEN INSERT (PATCH_NAME, INSTALL_DATE, STATUS)\
-                \nVALUES({patch.branch}, current_timestamp, 'SUCCESS')\
+                \nVALUES('{patch.branch}', current_timestamp, 'SUCCESS')\
                 \nWHEN MATCHED THEN UPDATE SET INSTALL_DATE=current_timestamp, STATUS='SUCCESS'"
                 print(add_to_install_patches)
                 print(bytes(add_to_install_patches, 'UTF-8'))
